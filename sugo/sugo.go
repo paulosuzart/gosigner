a1f7ba22afd073c5750a5736044a7730f3b1ad7e
paulosuzart <paulosuzart@gmail.com>
2011-06-12 15:49:07 -0300

    docs added

--- sugo/sugo.go
+++ sugo/sugo.go
@@ -7,11 +7,18 @@ import (
 	"json"
 )
 
+//A REST Resource.
 type Resource struct {
-	Accepts, Renders string
+	//Used by sugo.Context.Read and sugo.Context.Render
+        //to use the correct parse for http body. Ex.:
+        //"application/json"
+        Accepts, Renders string
+        //The Paths supported
 	GET, POST, PUT, DELETE   *Path
 }
 
+//The SuGo Context. Single point of interaction with appengine api and
+//Requst/ResponseWriter.
 type Context struct {
 	ctx      appengine.Context
 	w        http.ResponseWriter
@@ -19,15 +26,22 @@ type Context struct {
 	resource *Resource
 }
 
+//Represents the Path for a given resource
+//handler by a given HTTP Verb.
 type Path struct {
+        //Regext to match request
 	Pattern string
-	Handler SuGoHandler
+        //The actual function that handles request.
+        Handler SuGoHandler
 }
 
+//Any function that receives sugo.Context as arguments.
 type SuGoHandler func(c Context)
 
+//The single SuGo instance
 var _sugo *SuGo
         
+//Adds a resource to SuGo
 func  Add(r *Resource) {
 	if r.GET != nil {
 		_sugo.routes[r.GET.Pattern] = r
@@ -37,24 +51,29 @@ func  Add(r *Resource) {
 	}
 }
 
+//Forces SuGo to read a jSON body and put the resoult into
+//data.
 func (self *Context) ReadJSON(data interface{}) {
 	dec := json.NewDecoder(self.r.Body)
 	dec.Decode(data)
 }
 
+//Forces SuGo to write into w the data as JSON.
 func (self *Context) RenderJSON(data interface{}) {
 	self.w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(self.w).Encode(data)
 }
 
+//Renders the result based on Resouce.Renders attribute
 func (self *Context) Render(data interface{}) {
-
 	self.w.Header().Set("Content-Type", self.resource.Renders)
 	if self.resource.Renders == "application/json" {
 		json.NewEncoder(self.w).Encode(data)
 	}
 }
 
+//The SuGo struct that holds the root context (usually '/')
+//and the routes.
 type SuGo struct {
         root string
         routes map[string]*Resource
@@ -64,10 +83,13 @@ func Make(root string) {
         _sugo = &SuGo{root, map[string]*Resource{}}
 }
 
+//Register the main httpHandler provided by SuGo.
 func Start() {
         //TODO if started, not start again
         http.HandleFunc(_sugo.root, httpHandler)
 }
+
+//Receives the Http request and decides where to send it.        
 func httpHandler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	var ctx Context
@@ -83,10 +105,13 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 		resource.POST.Handler(ctx)
 	}
 }
+
+//Search in the SuGo.routes the correct resource.
 func (self *SuGo) findHandler(path string) *Resource {
 	log.Print(path)
 	return self.routes[path]
 }
+
 func ensurePOST(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
