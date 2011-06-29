d3f3f6742bf48b374208af11a43f47511606139d
paulosuzart <paulosuzart@gmail.com>
2011-06-29 00:17:09 -0300

    removing 'this'

--- static/javascripts/gosigner-master.js
+++ static/javascripts/gosigner-master.js
@@ -1,18 +1,28 @@
-
 var signView = {
     Key : ko.observable(''),
     Signature : ko.observable(''),
     Content : ko.observable(''),
     sign : function () {
        $.post(  'sign', 
-                ko.toJSON(this), 
+                ko.toJSON(signView), 
                 function(data){
                    ko.mapping.fromJS(data, {}, signView); 
                    $("#sign").css('visibility', 'visible');
                 });
     },
 };
-
+/**
+var App = { signView : signView, 
+            aboutView : aboutView,
+            about : false,
+            display : function(about) {
+              return about? "index_template" : "about_template";  
+            },
+            about : function(){
+                App.about = true;
+            }
+        }
+*/
 $(function(){
     ko.applyBindings(signView, document.getElementById('sandbar-form'));  
 });
