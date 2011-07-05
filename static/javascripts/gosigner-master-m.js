2ff71da9a06dfab69ec576b165bfbd728dea6c2f
paulosuzart <paulosuzart@gmail.com>
2011-07-04 23:52:17 -0300

    Added mobile version

---
+++ static/javascripts/gosigner-master-m.js
@@ -0,0 +1,17 @@
+var signView = {
+    Key : ko.observable('Type Here'),
+    Signature : ko.observable(''),
+    Content : ko.observable(''),
+    sign : function () {
+       $.post(  'sign', 
+                ko.toJSON(signView), 
+                function(data){
+                   ko.mapping.fromJS(data, {}, signView);
+                   //$("#sign").css('visibility', 'visible');
+                });
+    }
+};
+
+$(function(){
+    ko.applyBindings(signView);
+});
